88566d3a008acb3be58d410717d5ed09ae9c5f0d
sunny <mrsingh.someet@gmail.com>
2024-07-02 14:28:24 +0530

    web scrapper RAG

--- 14_rag_embeddings.py
+++ 14_rag_embeddings.py
@@ -3,12 +3,14 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
-from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "books", "odyssey.txt")
+file_path = os.path.join(current_dir, "books", "test.txt")
 db_dir = os.path.join(current_dir, "db")
+
+
 
 loader = TextLoader(file_path)
 documents = loader.load()
@@ -16,3 +18,67 @@ documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents=documents)
 
+print("\n--- Document Chunks Information ---")
+print(f"Number of document chunks: {len(docs)}")
+print(f"Sample chunk:\n{docs[0].page_content}\n")
+
+def create_vector_store(docs, embeddings, store_name):
+    persistent_directory = os.path.join(db_dir, store_name)
+    if not os.path.exists(persistent_directory):
+        print(f"\n--- Creating vector store {store_name} ---")
+        Chroma.from_documents(
+            docs, embeddings, persist_directory=persistent_directory)
+        print(f"--- Finished creating vector store {store_name} ---")
+    else:
+        print(
+            f"Vector store {store_name} already exists. No need to initialize.")
+
+
+
+# 1. OpenAI Embeddings
+# Uses OpenAI's embedding models.
+# Useful for general-purpose embeddings with high accuracy.
+# Note: The cost of using OpenAI embeddings will depend on your OpenAI API usage and pricing plan.
+# Pricing: https://openai.com/api/pricing/
+print("\n--- Using OpenAI Embeddings ---")
+embeddings = AzureOpenAIEmbeddings(azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"], api_key=os.environ["AZURE_OPENAI_KEY"],
+                                       openai_api_type="azure", azure_deployment="Embedding", show_progress_bar=True)
+create_vector_store(docs, embeddings, "chroma_db_openai")
+
+
+# 2. Hugging Face Transformers
+# Uses models from the Hugging Face library.
+# Ideal for leveraging a wide variety of models for different tasks.
+# Note: Running Hugging Face models locally on your machine incurs no direct cost other than using your computational resources.
+# Note: Find other models at https://huggingface.co/models?other=embeddings
+
+def query_vector_store(store_name, query, embedding_function):
+    persistent_directory = os.path.join(db_dir, store_name)
+    if os.path.exists(persistent_directory):
+        print(f"\n--- Querying the Vector Store {store_name} ---")
+        db = Chroma(
+            persist_directory=persistent_directory,
+            embedding_function=embedding_function,
+        )
+        retriever = db.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={"k": 3, "score_threshold": 0.1},
+        )
+        relevant_docs = retriever.invoke(query)
+        # Display the relevant results with metadata
+        print(f"\n--- Relevant Documents for {store_name} ---")
+        for i, doc in enumerate(relevant_docs, 1):
+            print(f"Document {i}:\n{doc.page_content}\n")
+            if doc.metadata:
+                print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
+    else:
+        print(f"Vector store {store_name} does not exist.")
+
+# Define the user's question
+query = "Who is ron?"
+
+# Query each vector store
+query_vector_store("chroma_db_openai", query, embedding_function=embeddings)
+# query_vector_store("chroma_db_huggingface", query, huggingface_embeddings)
+
+print("Querying demonstrations completed.")
